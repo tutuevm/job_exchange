f32ca613c0889d252d0857204802c4abefca1f6f
tutuevm <mxim.tutuev@gmail.com>
2024-07-15 09:37:44 +0400

    user registr method created

--- src/services/UserService.py
+++ src/services/UserService.py
@@ -1,7 +1,12 @@
-
-
+from src.schemas.UserSchema import UserSchema
+from src.utils.UnitOfWork import InterfaceUnitOfWork
+from src.auth.UserManager import UserManager
 
 class UserService:
 
-    async def register_user(self,):
-        pass
+    async def register_user(self, uow:InterfaceUnitOfWork, user: UserSchema) -> dict:
+        user_data = user.model_dump()
+        async with uow:
+            user_data['password'] = UserManager().hash_password(user_data['password'])
+            await uow.user.add_one(user_data)
+        return {"status": "OK"}
