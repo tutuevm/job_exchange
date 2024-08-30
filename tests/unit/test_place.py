136ceb44627229ff606178b0fd99c5e730e93ee0
tutuevm <mxim.tutuev@gmail.com>
2024-08-30 15:21:50 +0400

    create get all places test

--- tests/unit/test_place.py
+++ tests/unit/test_place.py
@@ -2,24 +2,35 @@ from uuid import UUID
 
 import pytest
 
+from src.database import async_session_maker
+from src.models import Place
 from src.schemas.PlaceSchema import PlaceSchema
 from src.services.PlaceService import PlaceService
 from src.utils.UnitOfWork import UnitOfWork
 
 
 class TestPlaceService:
+
     @pytest.mark.asyncio
     async def test_add_place(self):
-        place = PlaceSchema(
-            id="93169ada-d374-4152-addb-322412e335a3", title="Test Record"
-        )
+        place = PlaceSchema(id="93169ada-d374-4152-addb-322412e335a3", title="Ижевск")
         result = await PlaceService().add_place(UnitOfWork(), place=place)
         assert result == {"status": "OK"}
 
     @pytest.mark.asyncio
     async def test_get_place_by_id(self):
-        uow = UnitOfWork()
+        async with async_session_maker() as session:
+            new_record = Place(
+                id="16739ada-d374-4152-addb-322412e335a3", title="Test Record"
+            )
+            session.add(new_record)
+            await session.commit()
         result = await PlaceService().get_place_by_id(
-            uow=uow, id=UUID("93169ada-d374-4152-addb-322412e335a3")
+            uow=UnitOfWork(), id=UUID("16739ada-d374-4152-addb-322412e335a3")
         )
         assert len(result) == 1
+
+    @pytest.mark.asyncio
+    async def test_get_all(self):
+        result = await PlaceService().get_all(UnitOfWork())
+        assert type(result) == list
