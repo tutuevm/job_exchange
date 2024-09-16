78ca88fb66bc770b23ef85c55b3b242e3f73450d
tutuevm <mxim.tutuev@gmail.com>
2024-09-16 09:44:16 +0400

    create notification service

---
+++ src/Notification/services.py
@@ -0,0 +1,12 @@
+
+
+class NotificationService:
+
+    async def create_notification(self):
+        ...
+
+    async def get_user_unread_notifications(self):
+        ...
+
+    async def mark_read_notifications_list(self):
+        ...
