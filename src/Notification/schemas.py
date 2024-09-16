e3f8886cdb2c41d13cccadd6e13d73a5c1e5eef2
tutuevm <mxim.tutuev@gmail.com>
2024-09-16 09:48:09 +0400

    create notifications create schema

---
+++ src/Notification/schemas.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+class NotificationCreateSchema(BaseModel):
+    notification_data: str = Field(max_length=400)
+    user_id: UUID = Field()
