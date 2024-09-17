5dd6687f59fbb1bbbb235d43458a67f08b4e0c1a
tutuevm <mxim.tutuev@gmail.com>
2024-09-17 22:21:40 +0400

    small fix cors settings

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
         "*",
     ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
     allow_headers=["*"],
 )
 
