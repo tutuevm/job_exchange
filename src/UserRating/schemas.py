2b0d44e4ddfd90e5f990c4f6ba63ff45a8515324
tutuevm <mxim.tutuev@gmail.com>
2024-09-19 09:01:16 +0400

    update user rating schema, make comment field non required

--- src/UserRating/schemas.py
+++ src/UserRating/schemas.py
@@ -1,4 +1,3 @@
-from typing import Optional
 from uuid import UUID
 
 from pydantic import BaseModel, Field
@@ -7,4 +6,4 @@ from pydantic import BaseModel, Field
 class UserRatingCreateSchema(BaseModel):
     user_id: UUID = Field(description="Поле пользовтеля, которому выставляется рейтинг")
     rating_value: float = Field(le=5, ge=1)
-    comment: Optional[None | str] = Field(max_length=100, default=None)
+    comment: str = Field(max_length=100, default_factory=lambda: None)
